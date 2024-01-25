1c640ae37cc61742a0d7f3b7a701b67f6c7bb6ca
Vishnu7602 <vishnuvardhan7602@gmail.com>
2024-01-25 15:10:20 +0530

    pre commit

--- src/components/database.js
+++ src/components/database.js
@@ -1,27 +1,38 @@
 const mysql = require('mysql2/promise');
+const fs = require('fs')
 
 async function getPSConnection() {
-  console.log('DATABASE_URL2:', process.env.DATABASE_URL2);
-const url = new URL(process.env.DATABASE_URL2);
+  const url = new URL(process.env.DATABASE_URL2);
   
 
-
+  // Extract the connection details
   const host = url.hostname;
   const user = url.username;
   const password = url.password;
-  const database = url.pathname.replace(/^\//, ''); 
+  const database = url.pathname.replace(/^\//, ''); // Remove the leading slash
 
-  
-  const certificate = process.env.PLANETSCALE_CA_CERT;
+  // Get the certificate from the environment variable
+  // const certificate = process.env.PLANETSCALE_CA_CERT;
+  let certificate =''
+  if(process.env.NODE_ENV == 'production'){
+    certificate='/etc/pki/tls/certs/ca-bundle.crt'
+  }
+  else{
+    certificate ='/etc/ssl/cert.pem'
+  }
 
-  
+
+
+  // Create a new MySQL connection with SSL
   const connection = await mysql.createConnection({
     host,
     user,
     password,
     database,
     ssl: {
-      ca: Buffer.from(certificate, 'utf-8'),
+      // Convert the certificate string to a Buffer
+      // ca: Buffer.from(certificate, 'utf-8'),
+      ca: fs.readFileSync(certificate)
     },
   });
   
